6fd659473cb4303262a22cc5a6e22d82537a0bef
effozen <ashgrayblue0@gmail.com>
2024-10-29 00:53:57 +0900

    feat: mdx 컴포넌트처럼 사용 가능하게 설정

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,10 @@
 import { defineConfig } from 'vite';
+import mdx from '@mdx-js/rollup';
 import react from '@vitejs/plugin-react-swc';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [mdx(), react()],
   test: {
     globals: true, // 전역으로 Jest 스타일의 테스트 함수를 사용하도록 설정
     environment: 'jsdom', // 브라우저 환경을 시뮬레이션하도록 설정
@@ -11,6 +12,12 @@ export default defineConfig({
     coverage: {
       provider: 'istanbul', // 코드 커버리지 도구
       reporter: ['text', 'json', 'html'], // 커버리지 리포트 형식
+    },
+  },
+  resolve: {
+    alias: {
+      // 경로 별칭이 필요한 경우 추가
+      '@': '/src',
     },
   },
 });
