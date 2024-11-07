1dd3f7b99ed2371cb3363606736e2c11639e1f43
effozen <ashgrayblue0@gmail.com>
2024-11-07 09:19:04 +0900

    chore: docusaurus 문서 설정

---
+++ .docusaurus/client-modules.js
@@ -0,0 +1,6 @@
+export default [
+  require("/Users/zen/Dev/fantasmith/node_modules/.pnpm/infima@0.2.0-alpha.45/node_modules/infima/dist/css/default/default.css"),
+  require("/Users/zen/Dev/fantasmith/node_modules/.pnpm/@docusaurus+theme-classic@3.6.0_@types+react@18.3.12_acorn@8.14.0_react-dom@18.3.1_react@18.3_ufn6rfrq64m33xpwfz7zlzfwgm/node_modules/@docusaurus/theme-classic/lib/prism-include-languages"),
+  require("/Users/zen/Dev/fantasmith/node_modules/.pnpm/@docusaurus+theme-classic@3.6.0_@types+react@18.3.12_acorn@8.14.0_react-dom@18.3.1_react@18.3_ufn6rfrq64m33xpwfz7zlzfwgm/node_modules/@docusaurus/theme-classic/lib/nprogress"),
+  require("/Users/zen/Dev/fantasmith/src/css/custom.css"),
+];
