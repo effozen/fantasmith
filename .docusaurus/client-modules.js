1871a2085b42c37647d35b20e0df593a0bcfa50b
effozen <ashgrayblue0@gmail.com>
2024-11-10 01:32:54 +0900

    [Fix] : docusaurus 설정 관련 버그 수정 및 잡다한 파일 제거

--- .docusaurus/client-modules.js
+++
@@ -1,6 +0,0 @@
-export default [
-  require("/Users/zen/Dev/fantasmith/node_modules/.pnpm/infima@0.2.0-alpha.45/node_modules/infima/dist/css/default/default.css"),
-  require("/Users/zen/Dev/fantasmith/node_modules/.pnpm/@docusaurus+theme-classic@3.6.0_@types+react@18.3.12_acorn@6.4.2_react-dom@18.3.1_react@18.3._ri5deq4hoyq6jqvsoit33jqbb4/node_modules/@docusaurus/theme-classic/lib/prism-include-languages"),
-  require("/Users/zen/Dev/fantasmith/node_modules/.pnpm/@docusaurus+theme-classic@3.6.0_@types+react@18.3.12_acorn@6.4.2_react-dom@18.3.1_react@18.3._ri5deq4hoyq6jqvsoit33jqbb4/node_modules/@docusaurus/theme-classic/lib/nprogress"),
-  require("/Users/zen/Dev/fantasmith/src/css/custom.css"),
-];
