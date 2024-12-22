5c1e6a87ed210246d5224a0a6d7edac841a5697e
effozen <ashgrayblue0@gmail.com>
2024-12-23 06:29:42 +0900

    🎨 : 태그 레이아웃 수정

--- src/theme/DocTagDocListPage/index.tsx
+++ src/theme/DocTagDocListPage/index.tsx
@@ -13,6 +13,8 @@ import type {Props} from '@theme/DocTagDocListPage';
 import Unlisted from '@theme/ContentVisibility/Unlisted';
 import Heading from '@theme/Heading';
 
+import styles from './styles.module.css';
+
 // Very simple pluralization: probably good enough for now
 function useNDocsTaggedPlural() {
   const {selectMessage} = usePluralForm();
@@ -43,41 +45,48 @@ function usePageTitle(props: Props): string {
   );
 }
 
-function DocItem({doc}: {doc: Props['tag']['items'][number]}): JSX.Element {
+function DocItem({doc, docImg}: { doc: Props['tag']['items'][number], docImg:string }): JSX.Element {
 
   return (
-    <article className="margin-vert--lg">
+    <article className={clsx(styles.tagContainer)}>
       <Link to={doc.permalink}>
         <Heading as="h2">{doc.title}</Heading>
       </Link>
+      <div className={styles.docImgContainer}>
+        <img src={docImg} className={styles.docImg}/>
+      </div>
       {doc.description && <p>{doc.description}</p>}
     </article>
   );
 }
 
 function DocTagDocListPageMetadata({
-  title,
-  tag,
-}: Props & {title: string}): JSX.Element {
+                                     title,
+                                     tag,
+                                   }: Props & { title: string }): JSX.Element {
   return (
     <>
-      <PageMetadata title={title} description={tag.description} />
-      <SearchMetadata tag="doc_tag_doc_list" />
+      <PageMetadata title={title} description={tag.description}/>
+      <SearchMetadata tag="doc_tag_doc_list"/>
     </>
   );
 }
 
 function DocTagDocListPageContent({
-  tag,
-  title,
-}: Props & {title: string}): JSX.Element {
+                                    tag,
+                                    title
+                                  }: Props & { title: string }): JSX.Element {
+  console.log(tag.items);
+
+  // const img= tag
+
   return (
     <HtmlClassNameProvider
       className={clsx(ThemeClassNames.page.docsTagDocListPage)}>
       <div className="container margin-vert--lg">
         <div className="row">
           <main className="col col--8 col--offset-2">
-            {tag.unlisted && <Unlisted />}
+            {tag.unlisted && <Unlisted/>}
             <header className="margin-bottom--xl">
               <Heading as="h1">{title}</Heading>
               {tag.description && <p>{tag.description}</p>}
@@ -89,9 +98,12 @@ function DocTagDocListPageContent({
                 </Translate>
               </Link>
             </header>
-            <section className="margin-vert--lg">
-              {tag.items.map((doc) => (
-                <DocItem key={doc.id} doc={doc} />
+            {/*<section className="margin-vert--lg">*/}
+            <section className={clsx('row')}>
+              {tag.items.map((doc, index) => (
+                <article key={doc.id} className={clsx("col col--6", styles.tagList)}>
+                  <DocItem doc={doc} docImg={tag.items[index]["permalink"]}/>
+                </article>
               ))}
             </section>
           </main>
@@ -104,12 +116,12 @@ function DocTagDocListPageContent({
 export default function DocTagDocListPage(props: Props): JSX.Element {
   const title = `🔖 ${props.tag.label} 태그`;
 
-  console.log(props);
+  // console.log(props);
 
   return (
     <>
-      <DocTagDocListPageMetadata {...props} title={title} />
-      <DocTagDocListPageContent {...props} title={title} />
+      <DocTagDocListPageMetadata {...props} title={title}/>
+      <DocTagDocListPageContent {...props} title={title}/>
     </>
   );
 }
