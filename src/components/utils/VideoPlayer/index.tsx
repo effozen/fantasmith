20239b0bb9d2ab0212822217108c082c1b4bb2c6
effozen <ashgrayblue0@gmail.com>
2024-12-31 04:36:22 +0900

    ⚙️ : 비디오 컴포넌트 추가 및 검색 기능 추가

--- src/components/utils/VideoPlayer/index.tsx
+++ src/components/utils/VideoPlayer/index.tsx
@@ -1,21 +1,58 @@
+import styles from "./styles.module.css";
 import ReactPlayer from "react-player";
+import React, { useState, ReactNode, useEffect } from "react";
+import { useColorMode } from "@docusaurus/theme-common";
+import { useLocation } from "@docusaurus/router";
+import clsx from "clsx";
 
-export const VideoPlayer = ({ url, playing = false, muted = true }) => {
+interface IVideoPlayerProps {
+  readonly url: string;
+  readonly playing?: boolean;
+  readonly muted?: boolean;
+  readonly caption: string;
+  readonly href?: string;
+  readonly width?: string;
+}
+
+export const VideoPlayer = ({
+  url,
+  playing = false,
+  muted = true,
+  caption,
+  href,
+  width = "80%",
+}: IVideoPlayerProps) => {
+  const { colorMode } = useColorMode();
+  const location = useLocation();
+  const [captionAnchor, setCaptionAnchor] = useState<ReactNode>(caption);
+
+  useEffect(() => {
+    if (href) {
+      const anchor = React.createElement(
+        "a",
+        {
+          href,
+          target: "_blank",
+          rel: "noopener noreferrer",
+        },
+        caption
+      );
+      setCaptionAnchor(anchor);
+    }
+  }, []);
+
   return (
-    <div
-      style={{
-        display: "flex",
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <ReactPlayer
-        playing={playing}
-        muted={muted}
-        url={url}
-        controls
-        width="60%"
-      />
+    <div className={styles.videoContainer}>
+      <figure className={styles.videofigure}>
+        <ReactPlayer
+          playing={playing}
+          muted={muted}
+          url={url}
+          controls
+          width={width}
+        />
+        <figcaption className={styles.caption}>{captionAnchor}</figcaption>
+      </figure>
     </div>
   );
 };
---
+++ src/theme/CodeBlock/index.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import CodeBlock from "@theme-original/CodeBlock";
+import type CodeBlockType from "@theme/CodeBlock";
+import type { WrapperProps } from "@docusaurus/types";
+
+type Props = WrapperProps<typeof CodeBlockType>;
+
+export default function CodeBlockWrapper(props: Props): JSX.Element {
+  const title = props.className?.replace("language-", "");
+
+  return (
+    <>
+      <CodeBlock showLineNumbers title={title} {...props} />
+    </>
+  );
+}
