40692ccb6f7d0a7a776d529dbd51892771a56f38
effozen <ashgrayblue0@gmail.com>
2025-01-14 20:30:21 +0900

    📝 : 목차 요소 추가

---
+++ docs/frontEnd/tools/storybook/_utils_/seriesList.ts
@@ -0,0 +1,21 @@
+export const seriesList = {
+  title: 'Storybook',
+  children: [
+    {
+      name: '스토리북 개요',
+      link: '/frontEnd/tools/storybook/storybook_introduction/',
+    },
+    {
+      name: '스토리 파일을 뜯어보며 스토리북의 스토리 이해하기',
+      link: '/frontEnd/tools/storybook/look_at_storybook/',
+    },
+    {
+      name: '스토리북 작성 중 발생한 순환 참조 에러 해결하기: Dropdown 컴포넌트 사례 분석',
+      link: '/frontEnd/tools/storybook/circular_dependency_error/',
+    },
+    {
+      name: '스토리북에서 useContext 사용하기',
+      link: '/frontEnd/tools/storybook/how_to_use_useContext_on_storybook/',
+    },
+  ],
+};
