c28262aabe79af68f480573e81c73846ad9a70f8
effozen <ashgrayblue0@gmail.com>
2025-01-14 15:01:51 +0900

    📝 : 알고리즘 글에 목차 추가

---
+++ docs/skills/tutorial/__utils__/seriesList.ts
@@ -0,0 +1,25 @@
+export const seriesList = {
+  title: '알고리즘 및 자료구조 가이드',
+  children: [
+    {
+      name: '알고리즘과 자료구조에 대한 가이드',
+      link: '/skills/tutorial/',
+    },
+    {
+      name: '빅오 표기법(Big O Notation)이란?',
+      link: '/skills/tutorial/big_o/',
+    },
+    {
+      name: '알고리즘과 문제풀이법',
+      link: '/skills/tutorial/what_is_algorithm/',
+    },
+    {
+      name: '간단한 문제 풀이 테크닉',
+      link: '/skills/tutorial/simple_technique/',
+    },
+    {
+      name: '재귀(recursive) 알고리즘',
+      link: '/skills/tutorial/recursive/',
+    },
+  ],
+};
