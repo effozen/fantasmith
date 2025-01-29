aa869756ac29115544e7e18fb6925a52372bfeaa
effozen <ashgrayblue0@gmail.com>
2025-01-30 03:15:58 +0900

    🎨 : h5 css 수정

--- src/components/utils/Figure/index.tsx
+++ src/components/utils/Figure/index.tsx
@@ -1,30 +1,36 @@
-import {useLocation} from '@docusaurus/router';
+import { useLocation } from '@docusaurus/router';
 
 import styles from './styles.module.css';
-import React, {useState, useEffect, ReactNode} from "react";
-import {useColorMode} from "@docusaurus/theme-common";
-import clsx from "clsx";
+import React, { useState, useEffect, ReactNode } from 'react';
+import { useColorMode } from '@docusaurus/theme-common';
+import clsx from 'clsx';
 
 interface IFigureProps {
   readonly fileName: string;
   readonly alt: string;
   readonly caption: string;
   readonly href?: string;
+  readonly width?: string;
+  readonly height?: string;
 }
 
-export const Figure = ({fileName, alt, caption, href}: IFigureProps) => {
-  const {colorMode} = useColorMode();
+export const Figure = ({ fileName, alt, caption, href, width, height }: IFigureProps) => {
+  const { colorMode } = useColorMode();
   const location = useLocation();
   const path = `/media${location.pathname}img/${fileName}`;
   const [captionAnchor, setCaptionAnchor] = useState<ReactNode>(caption);
 
   useEffect(() => {
     if (href) {
-      const anchor = React.createElement('a', {
-        href,
-        target: '_blank',
-        rel: 'noopener noreferrer'
-      }, caption);
+      const anchor = React.createElement(
+        'a',
+        {
+          href,
+          target: '_blank',
+          rel: 'noopener noreferrer',
+        },
+        caption
+      );
       setCaptionAnchor(anchor);
     }
   }, []);
@@ -32,10 +38,13 @@ export const Figure = ({fileName, alt, caption, href}: IFigureProps) => {
   return (
     <div className={styles.figureContainer}>
       <figure className={styles.figure}>
-        <img className={clsx(styles.img, colorMode === 'light' ? styles.imgLight : styles.imgDark)} src={path}
-             alt={alt}/>
+        <img
+          className={clsx(styles.img, colorMode === 'light' ? styles.imgLight : styles.imgDark)}
+          src={path}
+          alt={alt}
+        />
         <figcaption className={styles.figcaption}>{captionAnchor}</figcaption>
       </figure>
     </div>
   );
-}
+};
