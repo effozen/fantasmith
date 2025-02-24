8d12d87d6d447e982898469c5355db244ffe8e7a
effozen <ashgrayblue0@gmail.com>
2025-02-25 01:26:16 +0900

    📝 : CSS 뿌수기 기초 작성

---
+++ docs/frontEnd/fundamental/css/joshwcomeau/_utils_/seriesList.ts
@@ -0,0 +1,13 @@
+export const seriesList = {
+  title: 'CSS 뿌수기',
+  children: [
+    {
+      name: 'CSS를 깊게 배워봐야겠다고 느낀 계기',
+      link: '/frontEnd/fundamental/css/joshwcomeau/introduction/',
+    },
+    {
+      name: 'CSS의 기본적인 룰',
+      link: '/frontEnd/fundamental/css/joshwcomeau/basic_rules/',
+    },
+  ],
+};
